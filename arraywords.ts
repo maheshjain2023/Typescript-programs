a8aba6c7f79e0dfe27ae962690bbecaca04f432b
Playwright-Automation <maheshjain2023@gmail.com>
2026-08-12 09:15:14 +0530

    Array program updated

--- arraywords.ts
+++ arraywords.ts
@@ -1,21 +1,23 @@
-let fruits:string[]=["apple", "banana", "orange", "apple", "kiwi", "banana", "grape"];
-let frequency: { [key: string]: number } = {};
+let fruits:string[]=["apple","banana","orange","banana","mango","apple"];
+let frequency:{[key:string]:number}={};
 for(let fruit of fruits)
 {
-    if(frequency[fruit])
-    {
-        frequency[fruit]++;
-    }
-    else
-    {
-        frequency[fruit]=1;
-    }
+ if(frequency[fruit])
+  { 
+    frequency[fruit]++;
+  }
+else
+{
+  frequency[fruit]=1;
 }
-        
+}
 for(let fruit in frequency)
 {
-    if(frequency[fruit]>1)
-    {
-        console.log(fruit+" : "+frequency[fruit]);
-    }
+  if(frequency[fruit]>1)
+{
+  console.log(fruit + ":" +frequency[fruit])
 }
+
+}
+
+export {};
