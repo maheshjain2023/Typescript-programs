62815d74932e5a945c3b5fab5c0a72537d6299a4
Playwright-Automation <maheshjain2023@gmail.com>
2026-08-12 08:58:17 +0530

    Paragraph program updated

--- paragrapwords.ts
+++ paragrapwords.ts
@@ -1,27 +1,24 @@
-let paragraph:string="Typescript is a strongly typed programming language that builds on JavaScript, giving you better tooling at any scale. It is a superset of JavaScript, which means that any valid JavaScript code is also valid TypeScript code. TypeScript adds optional static types, classes, and interfaces to JavaScript, allowing developers to catch errors early during development and improve code maintainability. With TypeScript, developers can write more robust and scalable applications while still leveraging the flexibility and ubiquity of JavaScript."
-let words:string[]=paragraph
-                .toLowerCase()
-                 .replace(/[.,!?]/g,"")
-                 .split(/\s+/);
-
-let frequency: { [key: string]: number } = {};
+let paragraph="Typescript is an easy language, typescript is a superset of JavaScript";
+let frequency:{[key:string]:number}={ };
+let words=paragraph
+          .toLowerCase()
+          .replace(/[.,!?]/g,"")
+          .split(/\s+/);
 for(let word of words)
+{
+  if(frequency[word])
+   {
+     frequency[word]++;
+   }
+   else
+   {
+     frequency[word]=1;
+   }
+}
+for(let word in frequency)
+{
+   if(frequency[word]>1)
     {
-        if(frequency[word])
-        {
-            frequency[word]++;
-        }
-
-        else
-        {
-            frequency[word]=1;
-        }
-    }                 
-    for(let word in frequency)
-        {
-            if(frequency[word]>1)
-            {
-                console.log(word+" : "+frequency[word]);
-            }
-        }
-    
+     console.log(word + ":" +frequency[word]);
+    }
+}
