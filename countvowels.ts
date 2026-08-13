2c3de0cba2424d4cd65375720c4fcc27c2ce7e04
Playwright-Automation <maheshjain2023@gmail.com>
2026-08-13 08:23:42 +0530

    Count vowels program added

---
+++ countvowels.ts
@@ -0,0 +1,16 @@
+
+let str:string="JavaScript";
+let count:number=0;
+for(let i=0; i<str.length;i++)
+{
+  let ch=str[i];
+   
+  if(ch=='a'|| ch=='e'||ch=='i'||ch=='o'||ch=='u')
+  {
+   count++;
+}
+
+}
+console.log(count);
+export{};
+
